4a3bb4691f072082210c9a2e9e29bd117101a120
Morphine <svetlana68-22@rambler.ru>
2020-03-11 23:40:19 +0300

    commit 1

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -6,7 +6,15 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
     public Bot(DefaultBotOptions options) {
@@ -28,22 +36,48 @@ public class Bot extends TelegramLongPollingBot {
         };
     }
 
-    /*public void sendMsg(Message message, String text){
+    public void sendMsg(Message message, String text){
         SendMessage sendMessage= new SendMessage();
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(message.getChatId().toString());
         sendMessage.setReplyToMessageId(message.getMessageId());
         sendMessage.setText(text);
         try{
-            sendMessage(sendMessage);
+            setButtons(sendMessage);
+            execute(sendMessage);
+
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
 
     }
-    */
+
 
 
     public void onUpdateReceived(Update update) {
-        /*Message message = update.getMessage();
+        Message message = update.getMessage();
+        /*if (update.hasMessage() && update.getMessage().hasText()) {
+
+        String user_first_name = update.getMessage().getChat().getFirstName();
+        String user_last_name = update.getMessage().getChat().getLastName();
+        String user_username = update.getMessage().getChat().getUserName();
+        long user_id = update.getMessage().getChat().getId();
+        String message_text = update.getMessage().getText();
+        long chat_id = update.getMessage().getChatId();
+
+        SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+        try {
+            execute(message); // Sending our message object to user
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }*/
+        if (message.equals("/start")) {
+            sendMsg(message, "Привет-привет!");
+        }
+
         if (message != null && message.hasText()){
             switch (message.getText()) {
                 case "/help":
@@ -56,7 +90,7 @@ public class Bot extends TelegramLongPollingBot {
 
             }
         }
-        if (update.hasMessage() && update.getMessage().hasText()) {
+        /*if (update.hasMessage() && update.getMessage().hasText()) {
                 SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
                         .setChatId(update.getMessage().getChatId())
                         .setText(update.getMessage().getText());
@@ -65,7 +99,27 @@ public class Bot extends TelegramLongPollingBot {
                 } catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
-            }*/
+            }
+
+            */
+
+    }
+
+    public void setButtons(SendMessage sendMessage){
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+
+        keyboardFirstRow.add(new KeyboardButton( "/help"));
+        keyboardFirstRow.add(new KeyboardButton( "/settings"));
+
+        keyboardRowList.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
 
     }
 
---
+++ src/main/java/User.java
@@ -0,0 +1,22 @@
+public class User {
+
+    public String tg_id ;
+    public String name ;
+    public Integer age ;
+    public String country ;
+    public String city;
+    public String gender;
+    public String photo;
+    public String tg_username;
+
+    /*public string tg_id { get; set; } //Уникальный айди пользователя
+    public string name { get; set; }//Отображаемое имя
+    public string age { get; set; }// Возраст
+    public string country { get; set; }// Страна
+    public string city { get; set; }//Город
+    public string gender { get; set; }//Пол
+    public string photo { get; set; }//Ссылка на фото
+    public string tg_username { get; set; }//Телеграмовский ник-нейм, по которому можно будет перейти к пользователю в личную переписку
+    public string tg_chat_id { get; set; }//Айди чата, куда отправлять ответ*/
+
+}
