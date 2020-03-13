5f14169537700392ca24d185bd15736ebd8d3b1f
Morphine <svetlana68-22@rambler.ru>
2020-03-13 20:54:27 +0300

    commit 1

--- src/main/java/SvBot/BotAbilit.java
+++ src/main/java/SvBot/BotAbilit.java
@@ -1,5 +1,18 @@
 package SvBot;
 
-public class BotAbilit {
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.bots.AbsSender;
 
+public class BotAbilit extends BotCommand {
+
+    public BotAbilit(String commandIdentifier, String description) {
+        super(commandIdentifier, description);
+    }
+
+    @Override
+    public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+
+    }
 }
